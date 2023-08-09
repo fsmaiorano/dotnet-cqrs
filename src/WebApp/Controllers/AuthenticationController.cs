48fc1729d9c5e1a67574db18f6ee15c592c311b9
Fábio Maiorano <fsmaiorano@outlook.com>
2023-08-09 02:10:22 +0100

    wip webapp

---
+++ src/WebApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class AuthenticationController : Controller
+{
+    private readonly ILogger<AuthenticationController> _logger;
+
+    public AuthenticationController(ILogger<AuthenticationController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult HandleAuthentication(string email, string password)
+    {
+        return View();
+    }
+}
